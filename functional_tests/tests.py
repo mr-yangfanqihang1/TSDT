8a8011eeafa66dd2792d8bba49695dae83c6e196
Duiqishishan <hw20040729@163.com>
2025-05-12 10:38:17 +0800

    v24:FT can test real_server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,6 +36,7 @@ import time
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 
@@ -43,6 +44,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url = 'http://' + real_server
         
     def tearDown(self):
         self.browser.quit()
